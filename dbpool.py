cffcbacdf093460408e6b565006e73ce17679b0e
芊芃 <chienen@qianpengdeMacBook-Air.local>
2023-09-02 08:27:46 +0800

    part1

--- dbpool.py
+++ dbpool.py
@@ -4,12 +4,12 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-mydb_password = os.getenv("DB_PASSWORD")
+# mydb_password = os.getenv("DB_PASSWORD")
 
 mydb = {
     "host": "localhost",
     "user": "chien",
-    "password": mydb_password,
+    "password": "Password1-",
     "database": "mydatabase",
     # "database": "attractions",
     # "user": "root",
